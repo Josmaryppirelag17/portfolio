5427a242a7200fdf846b8c9a86da9280d5c79fa0
Josmary Pirela <josmarypirela1712@outlook.com>
2024-04-20 02:02:06 -0400

    Corregida la barra lateral

--- src/Components/Navbar/Sidebar.jsx
+++ src/Components/Navbar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, useAnimation, AnimatePresence } from "framer-motion";
 import "./Sidebar.css";
 import home from "../../assets/house-chimney-solid.svg";
 import aboutme from "../../assets/user-solid.svg";
@@ -7,10 +8,12 @@ import formation from "../../assets/chalkboard-user-solid.svg";
 import portfolio from "../../assets/ranking-star-solid.svg";
 import contact from "../../assets/comments-regular.svg";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const Sidebar = () => {
   const [clicked, setClicked] = useState(true);
   const [activeIcon, setActiveIcon] = useState(null);
+  const sidebarControls = useAnimation();
 
   const handleClick = () => {
     setClicked(!clicked);
@@ -21,120 +24,120 @@ const Sidebar = () => {
     setClicked(true);
   };
 
+  useEffect(() => {
+    if (clicked) {
+      sidebarControls.start({
+        opacity: 0,
+        scale: 0.3,
+        x: -50,
+        transition: {
+          duration: 0.5,
+          ease: "easeInOut",
+        },
+      });
+    } else {
+      sidebarControls.start({
+        opacity: 1,
+        scale: 1,
+        x: 0,
+        transition: {
+          duration: 0.2,
+          ease: "easeInOut",
+        },
+      });
+    }
+  }, [clicked, sidebarControls]);
+
   return (
     <div className="sidebar">
-      <button
-        className={`sidebar__button ${clicked ? "clicked" : ""}`}
-        onClick={handleClick}
-      ></button>
-      <div className="sidebar__container">
-        <div className={`sidebar__text ${clicked ? "hidden" : "clicked"}`}>
-          <ul className="sidebar__list">
-            <Link
-              to="/"
-              className={`sidebar__icon ${
-                activeIcon === "home" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("home")}
-            >
-              {!clicked && (
-                <>
+      <AnimatePresence>
+        <motion.div
+          className="sidebar__container"
+          animate={{ width: 55, opacity: 1 }}
+          exit={{ with: 0, opacity: 0 }}
+        >
+          <div className={`sidebar__text`}>
+            <ul className="sidebar__list">
+              <motion.li
+                key="home"
+                initial={{ opacity: 0, scale: 0.3, x: -50 }}
+                animate={{ opacity: 1, scale: 1, x: 0 }}
+                exit={{ opacity: 0, scale: 0.3, x: -50 }}
+              >
+                <Link
+                  to="/"
+                  className={`sidebar__icon`}
+                  onClick={() => handleItemClick("home")}
+                >
                   <img className="sidebar__icon-img" src={home} alt="" />
-                  <span>Home</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={home} alt="" />
-              )}
-            </Link>
-            <Link
-              to="/aboutme"
-              className={`sidebar__icon ${
-                activeIcon === "aboutme" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("aboutme")}
-            >
-              {!clicked && (
-                <>
+                </Link>
+              </motion.li>
+              <motion.li
+                key="home"
+                initial={{ opacity: 0, scale: 0.3, x: -50 }}
+                animate={{ opacity: 1, scale: 1, x: 0 }}
+                exit={{ opacity: 0, scale: 0.3, x: -50 }}
+              >
+                <Link
+                  to="/aboutme"
+                  className={`sidebar__icon`}
+                  onClick={() => handleItemClick("aboutme")}
+                >
                   <img className="sidebar__icon-img" src={aboutme} alt="" />
-                  <span>Sobre mi</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={aboutme} alt="" />
-              )}
-            </Link>{" "}
-            <Link
-              to="formation"
-              className={`sidebar__icon ${
-                activeIcon === "formation" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("formation")}
-            >
-              {!clicked && (
-                <>
+                </Link>{" "}
+              </motion.li>
+              <motion.li
+                key="home"
+                initial={{ opacity: 0, scale: 0.3, x: -50 }}
+                animate={{ opacity: 1, scale: 1, x: 0 }}
+                exit={{ opacity: 0, scale: 0.3, x: -50 }}
+              >
+                {" "}
+                <Link
+                  to="formation"
+                  className={`sidebar__icon`}
+                  onClick={() => handleItemClick("formation")}
+                >
                   <img className="sidebar__icon-img" src={formation} alt="" />
-                  <span>Formacion</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={formation} alt="" />
-              )}
-            </Link>{" "}
-            <Link
-              to="/portfolio"
-              className={`sidebar__icon ${
-                activeIcon === "portfolio" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("portfolio")}
-            >
-              {!clicked && (
-                <>
+                </Link>{" "}
+              </motion.li>
+
+              <motion.li
+                key="home"
+                initial={{ opacity: 0, scale: 0.3, x: -50 }}
+                animate={{ opacity: 1, scale: 1, x: 0 }}
+                exit={{ opacity: 0, scale: 0.3, x: -50 }}
+              >
+                {" "}
+                <Link
+                  to="/portfolio"
+                  className={`sidebar__icon ${
+                    activeIcon === "portfolio" ? "active" : ""
+                  }`}
+                  onClick={() => handleItemClick("portfolio")}
+                >
                   <img className="sidebar__icon-img" src={portfolio} alt="" />
-                  <span>Portafolio</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={portfolio} alt="" />
-              )}
-            </Link>
-{/*            <Link
-              to="/blog"
-              className={`sidebar__icon ${
-                activeIcon === "blog" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("blog")}
-            >
-              {!clicked && (
-                <>
-                  <img className="sidebar__icon-img" src={blog} alt="" />
-                  <span>Blog</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={blog} alt="" />
-              )}
-              </Link>*/}
-            <Link
-              to="contact"
-              className={`sidebar__icon ${
-                activeIcon === "contact" ? "active" : ""
-              }`}
-              onClick={() => handleItemClick("contact")}
-            >
-              {!clicked && (
-                <>
+                </Link>
+              </motion.li>
+              <motion.li
+                key="home"
+                initial={{ opacity: 0, scale: 0.3, x: -50 }}
+                animate={{ opacity: 1, scale: 1, x: 0 }}
+                exit={{ opacity: 0, scale: 0.3, x: -50 }}
+              >
+                {" "}
+                <Link
+                  to="contact"
+                  className={`sidebar__icon`}
+                  onClick={() => handleItemClick("contact")}
+                >
                   <img className="sidebar__icon-img" src={contact} alt="" />
-                  <span>Contacto</span>
-                </>
-              )}
-              {clicked && (
-                <img className="sidebar__icon-img" src={contact} alt="" />
-              )}
-            </Link>
-          </ul>
-        </div>
-      </div>
+                </Link>
+              </motion.li>
+            </ul>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
